331f35c4fc72abcb476b61f7f5806e1ef962909b
ismael rios <isma.rios@live.com.mx>
2020-02-07 13:03:49 -0600

    Add socket.io, configure on front and backend, detect conections an disconnections

--- server/server.js
+++ server/server.js
@@ -1,20 +1,36 @@
 const express = require('express');
+const socketIO = require('socket.io')
+const http = require('http')
 
 const path = require('path');
 
 const app = express();
+//Definir el servidor para correr la app para que funcione con express
+let server = http.createServer(app)
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+//Inicializar Socket IO para mantener una conexión con el server
+let io = socketIO(server)
 
+io.on('connection', (client) => {
+    console.log('Usuario conectado');
 
-app.listen(port, (err) => {
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    })
+});
+
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
     console.log(`Servidor corriendo en puerto ${ port }`);
 
 });
+
+//Checar 
+//http://localhost:3000/socket.io/socket.io.js
